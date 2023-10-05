2daa1142858bc1c9b0540d36fc28aff41a785313
valentinapel <865173@stud.unive.it>
2023-10-05 17:41:17 +0200

    waitress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {Routes} from "@angular/router";
+import {WaitressComponent} from '.components/waitress/waitress.component';
 
-
+const routes: Routes [
+  { path: 'waitress', component: waitress}
+  ];
 
 @NgModule({
   declarations: [],
---
+++ src/app/components/waitress/waitress.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waitress',
+  templateUrl: './waitress.component.html',
+  styleUrls: ['./waitress.component.css']
+})
+export class WaitressComponent {
+
+}
