6b789389ac35bf47c3c0d387d9421727d6da2c40
valentinapel <865173@stud.unive.it>
2023-10-20 18:42:05 +0200

    creazione file server.js per collegare db e input del form, modifica html, NON FUNZIONANTE

--- server.js
+++ server.js
@@ -1,13 +1,59 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+/**
+ * @type {Mongoose}
+ */
+const mongoose = require('mongoose')
+var bodyParser=require("body-parser");
 
-// Definisci una route per la radice del sito (es. http://localhost:3000/)
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+mongoose.connect('mongodb+srv://usertaw:userpass@tawdb.1oresjm.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connessione a MongoDB avvenuta con successo');
+  })
+  .catch((err) => {
+    console.error('Errore nella connessione a MongoDB: ' + err);
+  });
 
-// Avvia il server Express.js sulla porta specificata (in questo caso, 3000)
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+var db=mongoose.connection;
+
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+  console.log("connection succeeded");
+})
+
+var app=express()
+
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/registrazione', function(req,res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var role =req.body.role;
+
+  var data = {
+    "name": username,
+    "password":password,
+    "role": role
+  }
+  db.collection('details').insertOne(data,function(err, collection){
+    if (err) throw err;
+    console.log("Record inserted Successfully");
+
+  });
+
+})
+
+
+
+
+
+console.log("server listening at port 3000");
+
+
