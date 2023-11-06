6818673c6c9c8c91383e13fb0570b829e6e88d77
valentinapel <865173@stud.unive.it>
2023-11-06 17:17:45 +0100

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit fd90d07922d938d5e8b1f0eda41ea3cc5ce51303, reversing
    changes made to aa46892d62236802aed6ef3dff013ad2adeb5ca5.

--- src/app/components/cooks/cooks.component.ts
+++ src/app/components/cooks/cooks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cooks',
@@ -6,27 +6,5 @@ import { Component, Renderer2, ElementRef } from '@angular/core';
   styleUrls: ['./cooks.component.css']
 })
 export class CooksComponent {
-
-  isNotificationVisible: boolean = false;
-
-  showNotification() {
-    this.isNotificationVisible = true;
-
-    setTimeout(() => {
-      this.isNotificationVisible = false;
-    }, 2000); // Disappear after 2 seconds
-  }
-  
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
-
-  openDetailModal(){
-    const modalElement = this.el.nativeElement.querySelector('#detailModal');
-    this.renderer.removeClass(modalElement, 'hidden');
-  }
-
-  closeDetailModal() {
-    const modalElement = this.el.nativeElement.querySelector('#detailModal');
-    this.renderer.addClass(modalElement, 'hidden');
-  }
 
 }
