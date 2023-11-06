2c935501731f98b90adc21d77d5ff7db460b223e
valentinapel <865173@stud.unive.it>
2023-11-06 17:17:34 +0100

    Revert "no working"
    This reverts commit aacee9f27a030d9b0bf3add9dd1aacd6c2ad8c05.

--- route/routes.js
+++ route/routes.js
@@ -2,11 +2,10 @@ var express =require('express');
 
 var userController =require ("../src/user/userController");
 
-//import {createRole} from "../src/user/roleController";
-
+//const roleController=require("/src/user/roleController")
 const router = express.Router();
 
-router.route('/user/role').post( createRole);
+//router.route('/user/role').post( roleController.createRole);
 
 router.route('/user/login').post(userController.loginUserControllerFn);
 router.route('/user/create').post(userController.createUserControllerFn);
