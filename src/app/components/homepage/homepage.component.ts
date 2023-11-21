4f7d4727221ee73408b329fe8cdcf5e4522c4b42
bakaren <46865323+bakaren@users.noreply.github.com>
2023-11-21 22:10:38 +0100

    Tentative implementation of signup notif

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {WaitressComponent} from "../waitress/waitress.component";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -7,6 +8,27 @@ import {WaitressComponent} from "../waitress/waitress.component";
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Access the data passed from the previous component
+    if(this.route.snapshot.paramMap.get('registered') === 'true'){
+      this.showNotification();
+      console.log("Wahoooo");
+    }
+    
+  }
+
+  isNotificationVisible: boolean = false;
+
+  showNotification() {
+    this.isNotificationVisible = true;
+
+    setTimeout(() => {
+      this.isNotificationVisible = false;
+    }, 5000);
+  }
 
   protected readonly WaitressComponent = WaitressComponent;
 }
