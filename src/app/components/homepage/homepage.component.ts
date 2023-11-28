6651e9608393e8f2d79e49914b26993f24acce22
bakaren <46865323+bakaren@users.noreply.github.com>
2023-11-28 17:46:01 +0100

    Remove unused code

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {WaitressComponent} from "../waitress/waitress.component";
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -9,26 +7,4 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomepageComponent {
 
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit() {
-    // Access the data passed from the previous component
-    if(this.route.snapshot.paramMap.get('registered') === 'true'){
-      this.showNotification();
-      console.log("Wahoooo");
-    }
-    
-  }
-
-  isNotificationVisible: boolean = false;
-
-  showNotification() {
-    this.isNotificationVisible = true;
-
-    setTimeout(() => {
-      this.isNotificationVisible = false;
-    }, 5000);
-  }
-
-  protected readonly WaitressComponent = WaitressComponent;
 }
